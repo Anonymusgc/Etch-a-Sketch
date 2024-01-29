8b64d19c0b16e8b1cb3b9b528ea711aebfc5b765
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-01-30 00:24:11 +0100

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 "use strict";
 
-const createGrid = function () {
+const createGrid = function (newSize = 16) {
   const container = document.querySelector(".container");
-  for (let i = 0; i < 256; i++) {
+  const cellArray = [];
+
+  const width = container.offsetWidth;
+  const height = container.offsetHeight;
+  let cellWidth = width / newSize;
+  let cellHeight = height / newSize;
+  // console.log(cellHeight);
+  // console.log(cellWidth);
+  // console.log(newSize * newSize);
+
+  for (let i = 0; i < newSize * newSize; i++) {
     const div = document.createElement("div");
     div.classList.add("gridCell");
-    container.appendChild(div);
-    // console.log("Work");
+
+    div.style.width = cellWidth + "px";
+    div.style.height = cellHeight + "px";
+    // container.appendChild(div);
+    cellArray.push(div);
+    console.log("Work");
   }
+  console.log(cellArray);
+  container.replaceChildren(...cellArray);
 };
 
 // function for changing background color by adding class
@@ -22,6 +38,21 @@ const addDraw = function () {
     cell.addEventListener("mouseover", draw);
   });
 };
+
+const changeGrid = function () {
+  const newSize = Number(
+    prompt("Input number of squares per side for new grid ( max 100 squares )")
+  );
+  if (newSize < 1 || newSize > 100) {
+    alert("Input out of range, please select again");
+    return changeGrid();
+  }
+  createGrid(newSize);
+  addDraw();
+};
+
+const changeGridBtn = document.querySelector(".change-grid");
+changeGridBtn.addEventListener("click", changeGrid);
 
 createGrid();
 addDraw();
