faefe6b7475310cccd72f1fdb10d133bfa0c502d
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-01-29 23:36:46 +0100

    Add hover effect

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const createGrid = function () {
   const container = document.querySelector(".container");
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.classList.add("gridCell");
     container.appendChild(div);
-    console.log("Work");
+    // console.log("Work");
   }
 };
+
+// function for changing background color by adding class
+const draw = function (event) {
+  event.target.classList.add("drawn");
+};
+
+// adding draw to each cell on hover
+const addDraw = function () {
+  const cells = document.querySelectorAll(".gridCell");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", draw);
+  });
+};
+
 createGrid();
+addDraw();
